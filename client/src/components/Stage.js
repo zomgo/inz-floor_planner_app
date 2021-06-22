9a2d18a00d0d48290bf0a4ef656dcc4c1b542d96
Grzegorz Kercz <grzegorz.kercz@gmail.com>
2021-06-22 09:45:59 +0200

    minor fixes i guess

--- client/src/components/Stage.js
+++ client/src/components/Stage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import { useContext } from 'react';
 import StageContext from '../store/stage-context';
